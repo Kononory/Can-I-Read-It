975572a552414d1c4e1785bc813883ce7f80621b
Mykola Kononchuk <57183295+Kononory@users.noreply.github.com>
2023-07-17 13:42:27 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("redirectButton").addEventListener("click", function() {
+    window.location.href = "https://rt.pornhub.com/view_video.php?viewkey=ph62e48e4e51b8c";
+  });
+  
