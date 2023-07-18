c3c5bac3ba69da0e00e6d1bc2e9e128d00434032
Mykola Kononchuk <57183295+Kononory@users.noreply.github.com>
2023-07-18 12:00:10 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 document.getElementById("redirectButton").addEventListener("click", function() {
-    window.location.href = "https://rt.pornhub.com/view_video.php?viewkey=ph62e48e4e51b8c";
+    window.location.href = "https://t.me/can_i_read_it_bot";
   });
   
